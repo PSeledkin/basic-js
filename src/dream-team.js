350d0bcae0187f1ca15ac2881cd6356ba16de299
Alex Avtushko <pseledkin@yandex.by>
2020-10-27 20:14:56 +0300

    feat: start basic-js task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  try {
+    return arr.filter(element => typeof(element) === 'string').map(element => element.replace(/\s/g, '')[0].toUpperCase()).sort().join('') || false; }
+    catch {
+      return false;
+    }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === null || date === undefined) {
+    return 'Unable to determine the time of year!'
+  }
+  try {
+if (date.getMonth() < 2 || date.getMonth() === 11 ) {
+  return 'winter';
+}
+else if (date.getMonth() < 5){
+  return 'spring';
+}
+else if (date.getMonth() < 8) {
+  return 'summer';
+}
+else if (date.getMonth() < 11) {
+  return 'autumn';
+}
+  }
+  catch {
+    throw "ERROR!!!";
+  }
 };
