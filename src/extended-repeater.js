465a1703b5f117df0e7447293146ece3d9d8d62d
Alex Avtushko <pseledkin@yandex.by>
2020-10-29 03:22:08 +0300

    feat: procced with basic-js task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  outStr = ""+str;
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (options.addition !== undefined) {
+    options.addition = String(options.addition);
+    if (!options.additionRepeatTimes) {
+      options.additionRepeatTimes = 1;
+    }
+    outAddition = "";
+    if (!options.additionSeparator) {
+      options.additionSeparator = "|";
+    }
+    i = 0;
+    do {
+      i++;
+      outAddition += options.addition;
+      if (options.additionRepeatTimes > 1 && i < options.additionRepeatTimes ) {
+        outAddition += options.additionSeparator;
+      }
+    } while (i < options.additionRepeatTimes);
+    outStr += outAddition;
+  }
+   outStr +=options.separator;
+
+  return outStr.repeat(options.repeatTimes).slice(0, -options.separator.length);
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('ERROR!')
+  }
+  transformArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--double-next":
+        if (arr[i + 1] !== undefined) {
+          transformArr.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev" :
+      if (arr[i - 1] !== undefined) {
+        if (arr[i-2] === '--discard-next') {
+          continue;
+        }
+        transformArr.push(arr[i - 1]);
+      }
+      break;
+      case '--discard-next' : if (arr[i + 1] !== undefined) {
+        i +=1;
+      }
+      break;
+      case '--discard-prev' : if (arr[i - 1] !== undefined) {
+        if (arr[i-2] === '--discard-next') {
+          continue;
+        }
+        transformArr.pop();
+      }
+      break;
+      default:
+        transformArr.push(arr[i]);
+    }
+  }
+  return transformArr;
 };
